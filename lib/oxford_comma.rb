0a8fc75bb6528302f04b66f4d5a486b1587530cc
Kevin Chi <kevinchi118@gmail.com>
2018-06-13 15:22:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-    array.join(", ")
+    if array.length == 1
+        return array[0]
+    if array.length == 2
+        return array.join(", and ")
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end 
 end
